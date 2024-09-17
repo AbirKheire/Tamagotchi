40361611323d5482d904bea4d37733a19317e97a
Attias <ilan.attias@free.fr>
2024-09-17 16:59:46 +0200

    ajoy index et css

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import { Tamagatchi } from './tamagatchi.js';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
 
+console.log('reussi')
 
 document.getElementById('begin').addEventListener('click', function() {
   
